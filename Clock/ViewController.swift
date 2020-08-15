1223b61257953942bdbba86fd41dff66eb264cfc
hk14004 <hardijs.kirsis@gmail.com>
2020-08-15 15:44:13 +0300

    Empty project

---
+++ Clock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by Hardijs on 15/08/2020.
+//  Copyright © 2020 Hardijs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
