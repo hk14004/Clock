7b097d0e200d6c4632b348bbddb829acad1552b4
hk14004 <hardijs.kirsis@gmail.com>
2020-08-17 23:51:48 +0300

    Add start button to main view Button stroke needs to be refactored as other buttons have same stroke. Need to add start button action.

--- Clock/TimerViewController.swift
+++ Clock/TimerViewController.swift
@@ -17,6 +17,8 @@ class TimerViewController: UIViewController {
         return view
     }()
     
+    private lazy var startButton: UIButton = UIButton()
+    
     private let minutesAndseconds: [Int] = makeIntArray(from: 0, to: 59)
     
     private let hours: [Int] = makeIntArray(from: 0, to: 23)
@@ -26,6 +28,7 @@ class TimerViewController: UIViewController {
         // Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor(named: "Secondary")
         setupTimePickerView()
+        setupStartButon()
     }
     
     func setupTimePickerView() {
@@ -36,6 +39,39 @@ class TimerViewController: UIViewController {
         timePickerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 15).isActive = true
         timePickerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -15).isActive = true
         timePickerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+    }
+    
+    func setupStartButon() {
+        // Configure button
+        startButton.setTitle("Start", for: .normal)
+        startButton.setTitleColor(.green, for: .normal)
+        startButton.layer.cornerRadius = 38
+        startButton.backgroundColor = UIColor(red: 26/255, green: 54/255, blue: 31/255, alpha: 1)
+        /// Add padded stroke
+        let strokeView = UIView()
+        strokeView.backgroundColor = .clear
+        strokeView.layer.borderColor = startButton.backgroundColor?.cgColor
+        strokeView.layer.borderWidth = 2.0;
+        strokeView.layer.cornerRadius = 40
+        /// Add start button as subview of stroke view
+        strokeView.addSubview(startButton)
+        
+        // Add button via stroke to main view
+         view.addSubview(strokeView)
+        
+        // Contraints
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        strokeView.translatesAutoresizingMaskIntoConstraints = false
+        /// Pin  start button to stroke view with a bit of padding
+        startButton.topAnchor.constraint(equalTo: strokeView.topAnchor, constant: 3).isActive = true
+        startButton.leadingAnchor.constraint(equalTo: strokeView.leadingAnchor, constant: 3).isActive = true
+        startButton.trailingAnchor.constraint(equalTo: strokeView.trailingAnchor, constant: -3).isActive = true
+        startButton.bottomAnchor.constraint(equalTo: strokeView.bottomAnchor, constant: -3).isActive = true
+        /// Align stroke view via button
+        strokeView.topAnchor.constraint(equalTo: timePickerView.bottomAnchor, constant: 100).isActive = true
+        strokeView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -15).isActive = true
+        strokeView.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        strokeView.heightAnchor.constraint(equalToConstant: 80).isActive = true
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
