9d61992c2b557f18db279460f0beee5e43b8e06c
hk14004 <hardijs.kirsis@gmail.com>
2020-09-01 00:25:23 +0300

    Add lap and start buttons in the UI

--- Clock/Scenes/Stopwatch/StopwatchViewController.swift
+++ Clock/Scenes/Stopwatch/StopwatchViewController.swift
@@ -17,9 +17,15 @@ class StopwatchViewController: UIViewController {
         return vc
     }()
     
+    private var lapButton: UIButton = UIButton()
+    
+    private var startButton: UIButton = UIButton()
+    
     override func viewDidLoad() {
         self.view.backgroundColor = UIColor(named: "Secondary")
         setupClocks()
+        setupLapButton()
+        setupStartButton()
     }
     
     private func setupClocks() {
@@ -31,6 +37,48 @@ class StopwatchViewController: UIViewController {
         clockFaceCollectionViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
         clockFaceCollectionViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
         clockFaceCollectionViewController.view.heightAnchor.constraint(equalToConstant: 300).isActive = true
+    }
+    
+    private func setupStartButton() {
+        // Configure button
+        startButton.setTitle("Start", for: .normal)
+        startButton.setTitleColor(.green, for: .normal)
+        startButton.layer.cornerRadius = 38
+        startButton.backgroundColor = UIColor(red: 26/255, green: 54/255, blue: 31/255, alpha: 1)
+        startButton.setBackgroundColor(color: UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.5), forState: .highlighted)
+        //startButton.addTarget(self, action: #selector(startButtonPressed), for: .touchUpInside)
+        startButton.addPaddedStroke(paddingColor: UIColor(named: "Secondary")!, strokeColor: startButton.backgroundColor!, borderWidth: 2)
+        
+        // Add button via stroke to main view
+        view.addSubview(startButton)
+        
+        // Contraints
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        startButton.topAnchor.constraint(equalTo: clockFaceCollectionViewController.view.bottomAnchor, constant: -60).isActive = true
+        startButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -15).isActive = true
+        startButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        startButton.heightAnchor.constraint(equalToConstant: 80).isActive = true
+    }
+    
+    private func setupLapButton() {
+        lapButton.setTitle("Lap", for: .normal)
+        lapButton.setTitleColor(.gray, for: .normal)
+        lapButton.layer.cornerRadius = 38
+        lapButton.backgroundColor = UIColor(named: "Primary")
+        lapButton.setBackgroundColor(color: UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.5), forState: .highlighted)
+
+        lapButton.addPaddedStroke(paddingColor: UIColor(named: "Secondary")!, strokeColor: lapButton.backgroundColor!, borderWidth: 2)
+        
+        
+        view.addSubview(lapButton)
+        
+        lapButton.translatesAutoresizingMaskIntoConstraints = false
+        lapButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
+        lapButton.topAnchor.constraint(equalTo: clockFaceCollectionViewController.view.bottomAnchor, constant: -60).isActive = true
+        lapButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
+         lapButton.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
